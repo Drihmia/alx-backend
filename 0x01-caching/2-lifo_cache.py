1e70dbe3e7cdcc32d254240ea78ad225e9859d91
Drihmia <drihmia.redouane@gmail.com>
2024-06-25 23:45:38 +0100

    fix: imporove logic

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module implements a FIFO cache.
+This module implements a LIFO cache.
 """
 from typing import Any
 from base_caching import BaseCaching
@@ -15,31 +15,29 @@ class LIFOCache(BaseCaching):
 
         self.__qeue = []
 
-    def put(self, key, item: Any) -> None:
+    def put(self, key: Any, item: Any) -> None:
         """A method that adds an item to the cache
         """
 
         if None in (key, item):
             return
 
-        self.__qeue.append(key)
-
         if key in self.__qeue:
             self.__qeue.pop(self.__qeue.index(key))
+        self.__qeue.append(key)
 
         if (len(self.cache_data.keys()) >= self.MAX_ITEMS and
                 key not in self.cache_data):
 
             discarded_key = self.__qeue[-2]
             self.__qeue.pop(-2)
-            # print(self.__qeue)
             del self.cache_data[discarded_key]
 
             print(f"DISCARD: {discarded_key}")
 
         self.cache_data.update({key: item})
 
-    def get(self, key):
+    def get(self, key: Any) -> Any:
         """ A method that gets an item from the cache
         """
 
