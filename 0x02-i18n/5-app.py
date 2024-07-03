159a4cd77cdc0610c51ff8858a893dc77f91ce4c
Drihmia <drihmia.redouane@gmail.com>
2024-07-03 01:33:55 +0100

    fix: put the code as suggested in the task even it is outdated ga

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -15,15 +15,8 @@ users = {
 app = Flask(__name__)
 
 
-# @babel.locale_selector
-def get_locale():
-    args = request.args
-    if "locale" in args and args.get('locale') in app.config['LANGUAGES']:
-        return args.get('locale')
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 def get_user():
+    """ Returns a user dictionary or None if not found """
     args = request.args
     if "login_as" in args:
         user_id = args.get("login_as")
@@ -32,24 +25,39 @@ def get_user():
 
 
 class Config:
+    """ Babel configuration class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
+
+
+@babel.locale_selector
+def get_locale():
+    """ Determine the best match for supported languages """
+    args = request.args
+    if "locale" in args:
+        locale = args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/')
 
 # @app_views.before_request
 @app_views.route('/', strict_slashes=False)
 def home():
+    """ Returns a simple HTML page """
     return render_template('5-index.html'), 200
 
 
 @app.before_request
 def before_request():
+    """ Executed before all request """
     g.user = get_user()
 
 
@@ -57,4 +65,4 @@ app.register_blueprint(app_views)
 # app.before_request(before_request)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
